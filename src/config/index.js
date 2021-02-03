b8245ec63a4d81f8d3ab7f14a3d8a3cc5c43be56
Sajal Shrestha <sajal.shres@gmail.com>
2021-02-03 22:29:53 +0545

    update config

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  author: "sajalshres",
+  siteTitle: "Sajal Shrestha Portfolio",
+  color: {
+    lightTheme: {},
+    darkTheme: {},
+  },
+  fonts: {},
+  social: [],
+  navLinks: [],
+  footerLinks: [],
+};
